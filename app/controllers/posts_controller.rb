c7212a4671cc69f54a905d271b15e3f0c3985067
Varun k <varunkothari007@gmail.com>
2023-05-17 17:05:26 +0530

    make post table to upload image and video

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,9 +13,13 @@ class PostsController < ApplicationController
     end
   end
 
+  def show
+  	@post = Post.find(params[:id])
+  end
+
   private
 
   def post_params
-    params.require(:post).permit(:title, :description, :image)
+    params.require(:post).permit(:title, :description, :image, :video)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
   has_one_attached :image
+  has_one_attached :video
 end
